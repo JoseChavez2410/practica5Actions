44d7c5a94e68da9dcc66ec1870c9749a273756a4
JChavez2410 <2410bautista@gmail.com>
2023-07-07 16:02:18 -0500

    version final

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ApostadorModule } from './student/apostador.module';
+import { ApostadorModule } from './apostador/apostador.module';
 
 @Module({
   imports: [
